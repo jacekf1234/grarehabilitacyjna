e5beab4ad0d959473ad89137026e72d8e56c6009
jacekf1234 <jacekf1234@gmail.com>
2014-06-06 00:14:16 +0200

    Add missing files

---
+++ src/pl/edu/agh/inz/gra1/User.java
@@ -0,0 +1,31 @@
+package pl.edu.agh.inz.gra1;
+
+public class User {
+	String login;
+	String name;
+	String surname;
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+}
